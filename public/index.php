343f083054bf9331a6bebc5194097d054a7b8d24
Cartoine <antoine.carrel@orange.fr>
2021-12-15 11:40:46 +0100

    create School, Classrooms & stuff

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@ spl_autoload_register(function ($classe) {
     include "../src/" . $classe . ".class.php";
 });
 
-// use App\School;
-// require_once('../vendor/autoload.php');
+
+require_once('../vendor/autoload.php');
 
 try {
-    $school = new School("rue de l'ecole", 20, 12, ["salut"]);
+    $school = new School("rue de l'ecole", 20, 12, ["Nom des classe"]);
 
     echo $school;
 } catch (Exception $e) {
@@ -18,6 +18,16 @@ try {
     $meuble = new Meuble(50, 50, 50, "tabouret");
 
     echo $meuble;
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
+try {
+    $classRooms = new ClassRooms("S_001", 20, 12, ["tous les meubles"]);
+
+    // $meuble->getNom()]
+    echo $classRooms;
 } catch (Exception $e) {
     echo $e->getMessage();
 }
--- src/classRooms.class.php
+++ src/classRooms.class.php
@@ -1,5 +1,21 @@
 <?php
 
-class ClassRoom extends InQuantity
+class ClassRooms extends InQuantity
 {
+
+    public function __construct(string $adresse, int $quantityMax, int $quantity, array $arrayContent)
+    {
+        parent::__construct($quantityMax, $quantity, $arrayContent);
+        $this->adresse = $adresse;
+    }
+
+    public function getInQuantity()
+    {
+    }
+
+    // Pour afficher 
+    public function __toString()
+    {
+        return self::class . "\nTaille de la sallles de classe maximum : " . $this->getQuantityMax() . "\nEspace occupé : " . $this->getQuantity() . "\nTous les meubles dans la salle : " . implode($this->getArrayContent());
+    }
 }
